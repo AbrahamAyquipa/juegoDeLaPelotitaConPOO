c34a78e193ca304b33d5e47f91fabb219510a921
Abrancito <103963261+SrGeek2000@users.noreply.github.com>
2022-09-03 13:50:25 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include "pch.h"
+#include "lanzador.h"
+#include <conio.h>
+
+using namespace System;
+
+int main() {
+	//tamaño de la consola
+	Console::SetWindowSize(80, 40);
+  //eliminar cursor
+	Console::CursorVisible = false;
+
+	//dibuja el lanzador
+	Console::ForegroundColor = ConsoleColor::Green;
+	Console::SetCursorPosition(30, 0); cout << "Juego de la pelota";
+	Console::ForegroundColor = ConsoleColor::White;
+
+	//define punteros
+	Lanzador* ObjetoLanzador;
+	Pelota* ObjetoPelota;
+	//crear instancia para usar los metodos publicos
+	ObjetoLanzador = new Lanzador();
+
+	while (true) {
+		//detectar presion en una tecla
+		if (_kbhit()) {
+			char tecla = getch();//captura la tecla presionada
+			tecla = toupper(tecla);
+			if (tecla == 'L') {
+				ObjetoPelota = new Pelota();
+				ObjetoLanzador->AgregarPelota(ObjetoPelota);
+			}
+		}
+		//muestra
+		if (ObjetoLanzador->getNumeroPelotas() > 0) {
+			ObjetoLanzador->MostrarPelota();
+		}
+		_sleep(100);
+	}
+	return 0;
+}
