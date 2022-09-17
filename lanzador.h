8340c72c14006cf375752a9b4d0211aaadbbce0f
Abrancito <103963261+AbrahamAyquipa@users.noreply.github.com>
2022-09-16 21:21:05 -0500

    Update lanzador.h

--- lanzador.h
+++ lanzador.h
@@ -1,56 +1,72 @@
-//clase controladora->controla el movimiento
-
 #pragma once
-#include "pelota.h"
-#include <iostream>
-using namespace std;
+#include "Pelota.h"
 
-class Lanzador {
+class Lanzador
+{
 private:
-	Pelota** arreglo_pelotas; //arreglo que contiene los atributos de pelota
-	int* numero_pelotas; //tamaño del arreglo
+	Pelota** arreglo;
+	int* N; //para el tam del arreglo
 public:
 	Lanzador();
 	~Lanzador();
-	//metodos de servicio
-	void AgregarPelota(Pelota* obj);
-	void MostrarPelota();
-	int getNumeroPelotas();
+	//metodos
+	void AgregarPelota(Pelota* objeto);
+	Pelota *ObtenerPelota(int index);
+	void MostrarPelotas();
+	int getN();
 };
-
-Lanzador::Lanzador() {
-	arreglo_pelotas = nullptr;//arreglo vacio
-	numero_pelotas = new int;//asignarle memoria
-	*numero_pelotas = 0;//apunta a 0
+//IMPLEMENTACION DE LA CLASE
+Lanzador::Lanzador()
+{
+	arreglo = nullptr;
+	N = new int;
+	*N = 0;//tam arreglo
 }
 
-Lanzador::~Lanzador() {}
+Lanzador::~Lanzador()
+{}
 
-void Lanzador::AgregarPelota(Pelota* objeto) {
-	//crear un arreglo temporal del mismo tipo  y tamaño + 1;
-	Pelota** temporal = new Pelota * [*numero_pelotas + 1];
-	//copiar todo el arreglo al temporal
-	if (arreglo_pelotas != nullptr) {
-		for (int i = 0; i < *numero_pelotas; i++)
-			temporal[i] = arreglo_pelotas[i];
+void Lanzador::AgregarPelota(Pelota* objeto)
+{
+	//crea un arreglo temporal del mismo tipo y tam +1
+	Pelota** temporal = new Pelota * [*N + 1];
+
+	//copia todo el arreglo al temporal
+	if(temporal!=nullptr)
+	{
+		for (int i = 0; i < *N; i++)
+		{
+			temporal[i] = arreglo[i];
+		}
 	}
-	//agregar el nuevo elemento
-	temporal[*numero_pelotas] = objeto;
-	//actualizar el tamaño del arreglo
-	*numero_pelotas = *numero_pelotas + 1;
-	//eliminar el arreglo original
-	if (arreglo_pelotas != nullptr) delete arreglo_pelotas;
-	arreglo_pelotas = temporal;
+	//agrega el nuevo elemento
+	temporal[*N] = objeto;
+
+	//actualiza el tam del arreglo
+	*N = *N + 1;
+	
+	//elimina el arreglo original
+	if(arreglo!=nullptr ) delete arreglo;
+
+	arreglo = temporal;
 }
 
-void Lanzador::MostrarPelota() {
-	for (int i = 0; i < *numero_pelotas; i++) {
-		arreglo_pelotas[i]->borrar();
-		arreglo_pelotas[i]->mover();
-		arreglo_pelotas[i]->dibujar();
+Pelota* Lanzador::ObtenerPelota(int index)
+{
+	return arreglo[index];
+}
+
+void Lanzador::MostrarPelotas()
+{
+	for (int i = 0; i < *N; i++)
+	{
+		arreglo[i]->borrar();
+		arreglo[i]->mover();
+		arreglo[i]->dibujar();
 	}
 }
 
-int Lanzador::getNumeroPelotas() {
-	return *numero_pelotas;
+int Lanzador::getN()
+{
+	return *N;
 }
